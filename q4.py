95f8e1a2ba40297d827fd0f2631951b6068e3257
amir <iamirranjbar@gmail.com>
2019-01-10 18:32:10 +0330

    q4 has been fixed.

--- q4.py
+++ q4.py
@@ -6,26 +6,31 @@ from numpy import fft
 from matplotlib import pyplot as plt
 from scipy import signal
 
-def show_fft(arr, name, L):
-	X = fft.fft(x)/L
-	plt.plot(X)
-	plt.title(name)
-	plt.grid()
-	plt.show()
+def nextpow2(x):
+	return (x-1).bit_length()
 
-fs , x = wavfile.read('sound.wav',False)
+def show_fft(arr, name, L):
+    NFFT = 2** nextpow2(L)
+    X = 2*np.absolute(fft.fft(arr, n=NFFT))/L
+    half = int(NFFT/2)
+    freq = fft.fftfreq(NFFT, d=1/fs)
+    plt.plot(freq[:half], X[:half])
+    plt.title(name)
+    plt.grid()
+    plt.show()
+
+fs , x = wavfile.read('sound.wav')
 print(len(x))
 print(fs)
-arr = np.array([0 for n in range(0, len(x))], float)
+arr = np.zeros(len(x))
 for i in range(0,len(x)):
-  a = (x[i][0] + x[i][1])/2
-  arr[i] = a
-carray = np.array(arr, dtype=np.int16)
-show_fft(carray, 'Before Downsample', fs)
+    a = (x[i][0] + x[i][1])/2
+    arr[i] = a
+before = np.array(arr, dtype=np.int16)
+show_fft(before, 'Before Downsample', len(x))
 arr = []
-for i in range(0,len(carray),3):
-  arr.append(carray[i])
-nparr = np.array(arr)
-show_fft(carray, 'After Downsample', fs/3)
-wavfile.write('result.wav', int(fs/3), nparr)
-	
+for i in range(0,len(before),3):
+  arr.append(before[i])
+after = np.array(arr)
+show_fft(after, 'After Downsample', int(len(x)/3))
+wavfile.write('result.wav', int(fs/3), after)
